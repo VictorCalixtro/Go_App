79db3f2a85a93ab109936d2c4c76fdba40c7fa93
Victor <victor@pop-os.localdomain>
2021-05-29 16:53:15 -0500

    added html files

--- cmd/web/routes.go
+++ cmd/web/routes.go
@@ -19,5 +19,8 @@ func routes(app *config.AppConfig) http.Handler {
 
 
 
+	fileServer := http.FileServer(http.Dir("./static/"))
+	mux.Handle("/static/*", http.StripPrefix("/static", fileServer))
+
 	return mux
 }
